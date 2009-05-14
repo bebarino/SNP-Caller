84496b75f5b0fb2c8f8f828746eac3b9e28cdc96
Stephen Boyd <bebarino@gmail.com>
2009-05-13 20:47:05 -0700

    Add read generator
    Generates reads of any length with a specified amount of coverage from a
    reference FASTA sequence.

---
+++ readGen.py
@@ -0,0 +1,63 @@
+#!/bin/env python
+
+import random
+import sys
+from optparse import OptionParser
+
+def gen_reads(seq, l, coverage=1):
+    num_reads = int((len(seq)/l) * coverage)
+    for _ in xrange(num_reads):
+        yield gen_read(seq, l)
+
+def gen_read(seq, l):
+    """Generates a random read of length 'l' from the sequence 'seq'"""
+    start = random.choice(xrange(len(seq)-l))
+    read = seq[start:start+l]
+    return (read, start)
+
+def write_reads(rs, f):
+    """Write reads 'rs' to a file 'f'"""
+    for read in rs:
+        f.write('\t'.join(str(x) for x in read))
+        f.write('\n')
+
+def parse_seq(f):
+    """Parse a FASTA sequence from the file 'f', returning its full string"""
+    if not f.readline().startswith(">"):
+        raise Exception("Not a FASTA sequence")
+    lines = []
+    for line in f:
+        if line.startswith(">"):
+            break
+        lines += [line.strip()]
+    return ''.join(lines)
+
+def parser_open_file(option, opt_str, value, parser, mode):
+    try:
+        f = open(value, mode)
+    except IOError, e:
+         parser.error(str(e))
+    setattr(parser.values, option.dest, f)
+
+if __name__ == "__main__":
+    parser = OptionParser(usage="%prog length [options]")
+
+    parser.add_option('-f', '--file', dest="infile", default=sys.stdin,
+                       help="use reference sequence from FILE", metavar="FILE",
+                       type=str, action="callback", callback=parser_open_file,
+                       callback_args=('r',))
+    parser.add_option('-o', '--out-file', dest='outfile', default=sys.stdout,
+                       help="write reads to FILE", metavar="FILE", type=str,
+                       action="callback", callback=parser_open_file,
+                       callback_args=('w',))
+    parser.add_option('-c', '--coverage', dest='coverage', default=1,
+                       help="amount of read coverage", metavar="NUM", type=int)
+
+    (options, args) = parser.parse_args(sys.argv[1:])
+    if len(args) != 1:
+	parser.error('length must be specified')
+    if not args[0].isdigit():
+        parser.error('length must be a digit')
+
+    write_reads(gen_reads(parse_seq(options.infile), int(args[0]),
+                options.coverage), options.outfile)
