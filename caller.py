277b9aa7c4709b164b62f683bc2fba2ac56aad69
Stephen Boyd <bebarino@gmail.com>
2009-05-18 15:39:37 -0700

    caller: implement more accurate consensus method

--- caller.py
+++ caller.py
@@ -3,6 +3,18 @@
 import sys
 from common import *
 
+class SNP(dict):
+    """Maintains a tally of SNPs occuring at a single position"""
+    def __init__(self, pos):
+        dict.__init__(self)
+        self.total = 0
+        self.pos = pos
+
+
+    def __setitem__(self, key, value):
+        dict.__setitem__(self, key, value)
+        self.total += 1
+
 def count_snps(reads, ref, snps={}):
     """Tabulate all snps and their positions"""
     for read in reads:
@@ -10,17 +22,25 @@ def count_snps(reads, ref, snps={}):
         start = int(start)
         for (i, (r, s)) in enumerate(zip(ref[start:], read)):
             if r != s:
-                stat = snps.get(start+i,{})
-                if s in stat:
-                    stat[s] += 1
+                snp = snps.setdefault(start+i,SNP(start+i))
+                if s in snp:
+                    snp[s] += 1
                 else:
-                    stat[s] = 1
-                snps[start+i] = stat
-    return snps
+                    snp[s] = 1
+    return snps.values()
 
-def consensus(snps):
-    """Return the snp with the highest frequency at each position"""
-    return [(max(snp), pos) for pos, snp in snps.iteritems()]
+def consensus(snp):
+    """Consensus snp calling method, returns "Unknown" if no majority"""
+    best = ('Unknown', 0)
+    for k, v in snp.iteritems():
+        p = float(v/snp.total)
+        if p > best[1]:
+            best = (k, v)
+    else:
+        if best[1] < 0.5:
+            best = ('Unknown', 0)
+    return best[0]
 
 if __name__ == "__main__":
-    print consensus(count_snps(sys.stdin, parse_seq(open(sys.argv[1], 'r'))))
+    snps = count_snps(sys.stdin, parse_seq(open(sys.argv[1], 'r')))
+    print '\n'.join("%s %s" % (snp.pos, consensus(snp)) for snp in snps)
