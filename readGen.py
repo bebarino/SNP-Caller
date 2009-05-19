9e8ef5abf87c61c934c0a5a1144a82da161e8d95
Stephen Boyd <bebarino@gmail.com>
2009-05-18 17:05:14 -0700

    readGen: introduce error option
    using --error (or -e) you can set the probability that a read will
    contain an error. An error in a read is simply a read having a
    wrong value for a position. The default for -e is 0, so that reads
    can be generated as they were genereated before.

--- readGen.py
+++ readGen.py
@@ -5,15 +5,22 @@ import sys
 from optparse import OptionParser
 from common import *
 
-def gen_reads(seq, l, coverage=1):
+def gen_reads(seq, l, coverage=1, e=0):
     num_reads = int((len(seq)/l) * coverage)
     for _ in xrange(num_reads):
-        yield gen_read(seq, l)
+        yield gen_read(seq, l, e)
 
-def gen_read(seq, l):
+def gen_read(seq, l, e):
     """Generates a random read of length 'l' from the sequence 'seq'"""
     start = random.choice(xrange(len(seq)-l))
     read = seq[start:start+l]
+    if random.random() < e:
+        # Make an error
+        pos = random.randrange(l) 
+        val = choice = read[pos]
+        while val == choice:
+            choice = random.choice(read)
+        read[pos] = choice
     return (read, start)
 
 def write_reads(rs, f):
@@ -36,6 +43,9 @@ if __name__ == "__main__":
     parser.add_option('-c', '--coverage', dest='coverage', default=1,
                        help="amount of read coverage", metavar="NUM",
                        type=float)
+    parser.add_option("-e", "--error", dest="prob",
+                        help="probability to introduce an error",
+                        default=0, type=float)
 
     (options, args) = parser.parse_args(sys.argv[1:])
     if len(args) != 1:
@@ -44,4 +54,4 @@ if __name__ == "__main__":
         parser.error('length must be a digit')
 
     write_reads(gen_reads(parse_seq(options.infile), int(args[0]),
-                options.coverage), options.outfile)
+                options.coverage, options.prob), options.outfile)
