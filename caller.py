7d36f4f65e15ed2ce38ea7e6492c52223e52ce77
Stephen Boyd <bebarino@gmail.com>
2009-05-18 17:03:19 -0700

    caller: A correct, but naive consensus method
    We cannot simply look for differences and record them, we must make
    a record for each location so we know the coverage (number of reads)
    for that position.

--- caller.py
+++ caller.py
@@ -5,15 +5,14 @@ from common import *
 
 class SNP(dict):
     """Maintains a tally of SNPs occuring at a single position"""
-    def __init__(self, pos):
+    def __init__(self, pos, ref):
         dict.__init__(self)
         self.total = 0
         self.pos = pos
-
+        self.ref = ref
 
     def __setitem__(self, key, value):
         dict.__setitem__(self, key, value)
-        self.total += 1
 
 def count_snps(reads, ref, snps={}):
     """Tabulate all snps and their positions"""
@@ -21,13 +20,14 @@ def count_snps(reads, ref, snps={}):
         read, start = read.split()
         start = int(start)
         for (i, (r, s)) in enumerate(zip(ref[start:], read)):
+            snp = snps.setdefault(start+i,SNP(start+i, r))
             if r != s:
-                snp = snps.setdefault(start+i,SNP(start+i))
                 if s in snp:
                     snp[s] += 1
                 else:
                     snp[s] = 1
-    return snps.values()
+            snp.total += 1
+    return snps
 
 def consensus(snp):
     """Consensus snp calling method, returns "Unknown" if no majority"""
@@ -39,8 +39,9 @@ def consensus(snp):
     else:
         if best[1] < 0.5:
             best = ('Unknown', 0)
-    return best[0]
+    return "%s %s" % (snp.pos, best[0])
 
 if __name__ == "__main__":
     snps = count_snps(sys.stdin, parse_seq(open(sys.argv[1], 'r')))
-    print '\n'.join("%s %s" % (snp.pos, consensus(snp)) for snp in snps)
+    print '\n'.join("%s" % consensus(snp) for snp in snps.values()
+                    if len(snp) > 0)
